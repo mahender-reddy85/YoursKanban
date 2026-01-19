a4a0f4e191903ca559166d867f8cc5fc0efec8d8
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2026-01-19 11:02:32 +0530

    no react

--- frontend/src/components/index.js
+++
@@ -1,7 +0,0 @@
-// Layout Components
-export { default as Layout } from './layout/Layout';
-export { default as Header } from './layout/Header';
-
-// Common Components (add more as needed)
-// export { default as Button } from './common/Button';
-// export { default as Input } from './common/Input';
