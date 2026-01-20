0bb9d5459848f96bdf6b1ddd0b346588f74aa9e1
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2026-01-20 19:14:08 +0530

    .

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+import app from './api/index.js';
+
+const PORT = process.env.PORT || 3001;
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`API Documentation: http://0.0.0.0:${PORT}/api-docs`);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.error(err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
